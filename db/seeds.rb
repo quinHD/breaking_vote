caa5d6209daabdf0d7e8e4f6be4c72ad357aba1c
vagrant <vagrant@ironhack>
2015-05-30 13:45:46 +0000

    intruction to load the full files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-towns = SmarterCSV.process('public/src_files/min_town_data.csv')
+towns = SmarterCSV.process('public/src_files/town_data.csv')
 
 towns.each do |town|
 	town.delete :fecha
@@ -9,7 +9,7 @@ towns.each do |town|
 
 end
 
-parties = SmarterCSV.process('public/src_files/min_party_data.csv')
+parties = SmarterCSV.process('public/src_files/party_data.csv')
 
 parties.each do |party|
 	town= Town.find_by town_code: party[:town_code]
