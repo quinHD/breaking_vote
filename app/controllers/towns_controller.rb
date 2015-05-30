8e020d372426f5d5ac5bd2f7e31730806f5e8e74
vagrant <vagrant@ironhack>
2015-05-30 14:52:20 +0000

    trying cors

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,10 +1,13 @@
 class TownsController < ApplicationController
-	allow_cors :index, :other_methods
-
+	
 	def index
 		towns = Town.all
 
 		respond_to do |format|
+		    headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  headers['Access-Control-Max-Age'] = "1728000"
       format.json{ render json: towns, include: :parties}
 		end
 	end
@@ -13,6 +16,10 @@ class TownsController < ApplicationController
 		town = Town.find_by town_code: params[:id]
 
 		respond_to do |format|
+			  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  headers['Access-Control-Max-Age'] = "1728000"
       format.json{ render json: town, include: :parties}
 		end
 	end
